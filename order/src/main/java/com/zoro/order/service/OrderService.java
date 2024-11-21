a049c504c1f31fafe88a9728744f1de593bf0045
Khanh Du <luongkhanhdu95@gmail.com>
2024-11-21 16:45:20 +0700

    add FeignClient and refactor container name

--- order/src/main/java/com/zoro/order/service/OrderService.java
+++ order/src/main/java/com/zoro/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.zoro.order.service;
 
+import com.zoro.order.client.InventoryClient;
 import com.zoro.order.dto.OrderRequest;
 import com.zoro.order.model.Order;
 import com.zoro.order.repository.OrderRepository;
@@ -13,14 +14,21 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = Order.builder()
-                .orderNumber(UUID.randomUUID().toString())
-                .price(orderRequest.price())
-                .skuCode(orderRequest.skuCode())
-                .quantity(orderRequest.quantity())
-                .build();
-        orderRepository.save(order);
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (isProductInStock) {
+            Order order = Order.builder()
+                    .orderNumber(UUID.randomUUID().toString())
+                    .price(orderRequest.price())
+                    .skuCode(orderRequest.skuCode())
+                    .quantity(orderRequest.quantity())
+                    .build();
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is out of stock.");
+        }
     }
 }

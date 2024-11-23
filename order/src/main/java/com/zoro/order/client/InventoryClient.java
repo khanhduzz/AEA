ced0ce9bd7b4f007ccce14492cf12306eba82a76
Khanh Du <luongkhanhdu95@gmail.com>
2024-11-23 10:12:25 +0700

    init store front-end

--- order/src/main/java/com/zoro/order/client/InventoryClient.java
+++ order/src/main/java/com/zoro/order/client/InventoryClient.java
@@ -4,11 +4,8 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
-import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.ServerResponse;
 
 public interface InventoryClient {
 

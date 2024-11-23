31aeed44cffeb186e38bd990006a8af6da4c6ed1
Khanh Du <luongkhanhdu95@gmail.com>
2024-11-23 09:59:45 +0700

    circut breaker for gateway and order to inventory

--- order/src/main/java/com/zoro/order/client/InventoryClient.java
+++ order/src/main/java/com/zoro/order/client/InventoryClient.java
@@ -1,10 +1,26 @@
 package com.zoro.order.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
 
 public interface InventoryClient {
 
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
+
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String skuCode, Integer quantity, Throwable throwable) {
+        log.info("Cannot get inventory for skuCode {}, failure reason: {}", skuCode, throwable.getMessage());
+        return false;
+    }
 }
